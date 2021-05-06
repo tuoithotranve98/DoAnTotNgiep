2581619c15d39fab2546f49aa842157ea5388bd2
bui manh <manhbd@sapo.vn>
2021-05-06 18:01:00 +0700

    fix cors

--- qlsc_backend/gateway/src/main/java/com/doan/gateway/GatewayApplication.java
+++ qlsc_backend/gateway/src/main/java/com/doan/gateway/GatewayApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
@@ -14,8 +17,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableDiscoveryClient
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
-	}
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
--- qlsc_frontend/src/constants/api.js
+++ qlsc_frontend/src/constants/api.js
@@ -3,7 +3,7 @@ export const API_ENDPOINT = 'http://localhost:8080/admin'
 export const API_USER = 'http://localhost:8085/admin'
 export const API_USER_AUTH = 'http://localhost:8085/auth'
 export const API_GATEWAY = 'http://localhost:8762'
-export const API_CUSTOMER = `${API_GATEWAY}/customer/admin`
+export const API_CUSTOMER = `${API_GATEWAY}/api/customer/admin`
 
 export const STATUS_CODE = {
     SUCCESS: 200,
