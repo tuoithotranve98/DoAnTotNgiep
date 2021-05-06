e94a1beb04b56f47080011161b37b275e491e371
bui manh <manhbd@sapo.vn>
2021-05-06 22:59:28 +0700

    edit path

--- qlsc_frontend/src/constants/api.js
+++ qlsc_frontend/src/constants/api.js
@@ -1,9 +1,12 @@
 export const PORT = 'http://localhost:8686'
 export const API_ENDPOINT = 'http://localhost:8080/admin'
-export const API_USER = 'http://localhost:8085/admin'
 export const API_USER_AUTH = 'http://localhost:8085/auth'
-export const API_GATEWAY = 'http://localhost:8762'
-export const API_CUSTOMER = `${API_GATEWAY}/api/customer/admin`
+export const API_GATEWAY = 'http://localhost:8762/api'
+export const API_USER = `${API_GATEWAY}/user/admin`
+export const API_CUSTOMER = `${API_GATEWAY}/customer/admin`
+export const API_PRODUCT = `${API_GATEWAY}/product/admin`
+export const API_IMAGE = `${API_GATEWAY}/image/admin`
+export const API_MAINTENANCECARD = `${API_GATEWAY}/maintenancecard/admin`
 
 export const STATUS_CODE = {
     SUCCESS: 200,
