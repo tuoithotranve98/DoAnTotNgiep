cee3fccdd78f245e3c0e6646119fbb96f84576ee
bui manh <manhbd@sapo.vn>
2021-05-18 08:30:03 +0700

    edit

--- qlsc_frontend/src/index.js
+++ qlsc_frontend/src/index.js
@@ -1,19 +1,18 @@
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {
-  BrowserRouter as Router
-} from 'react-router-dom'
-import { Provider } from 'react-redux'
-import configureStore from './store/reduxStore'
-import App from './app'
-import 'styles/main.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/reduxStore';
+import App from './app';
+import 'styles/main.scss';
+import history from './utils/history';
 
 const store = configureStore()
 function Root () {
   return (
     <Provider store={store}>
-      <Router basename="">
+      <Router history={history} basename="">
         <App />
       </Router>
     </Provider>
